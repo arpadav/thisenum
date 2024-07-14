00fbe77c245dd6fef79d706040e65dcb93a3981e
arpadav <arpadav@gmail.com>
2024-07-13 21:38:29 -0400

    adding doc comments, polishing

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 // --------------------------------------------------
 // external
 // --------------------------------------------------
+use quote::{
+    quote,
+    ToTokens,
+};
 use syn::{
     Meta,
     Data,
@@ -11,24 +15,44 @@ use syn::{
     MetaNameValue,
     parse_macro_input,
 };
-use quote::{
-    quote,
-    ToTokens,
-};
+use thiserror::Error;
 use proc_macro::TokenStream;
+
+#[derive(Error, Debug)]
+/// All errors that can occur while deriving [`EnumConst`]
+/// or [`EnumConstAny`]
+enum Error {
+    #[error("`{0}` can only be derived for enums")]
+    DeriveForNonEnum(String),
+    #[error("Missing #[armtype = ...] attribute {0}, required for `{1}`-derived enum")]
+    MissingArmType(String, String),
+    #[error("Missing #[value = ...] attribute, expected for `{0}`-derived enum")]
+    MissingValue(String),
+    #[error("Attemping to parse non-literal attribute for `value`: not yet supported")]
+    NonLiteralValue,
+}
 
 #[proc_macro_derive(EnumConst, attributes(value, armtype))]
 /// Add's constants to each arm of an enum
 /// 
-/// To get the value, call the function [`<enum_name>::value`]
+/// * To get the value as a reference, call the function [`<enum_name>::value`]
+/// * However, direct comparison to non-reference values are possible with
+///   [`PartialEq`]
+/// 
+/// The `#[armtype = ...]` attribute is required for this macro to function, 
+/// and must be applied to **the enum**, since all values share the same type.
+/// 
+/// All values set will return a [`&'static T`] reference. To the input type,
+/// of [`T`] AND [`&T`]. If multiple references are used (e.g. `&&T`), then
+/// the return type will be [`&'static &T`].
 /// 
 /// # Example
 /// 
 /// ```
 /// use enum_const::EnumConst;
 /// 
-/// #[derive(EnumConst)]
-/// #[armtype("i32")]
+/// #[derive(EnumConst, Debug)]
+/// #[armtype(i32)]
 /// enum MyEnum {
 ///     #[value = 0]
 ///     A,
@@ -36,7 +60,7 @@ use proc_macro::TokenStream;
 ///     B,
 /// }
 /// 
-/// #[derive(EnumConst)]
+/// #[derive(EnumConst, Debug)]
 /// #[armtype(&[u8])]
 /// enum Tags {
 ///     #[value = b"\x00\x01\x7f"]
@@ -48,14 +72,21 @@ use proc_macro::TokenStream;
 /// }
 /// 
 /// fn main() {
-///     assert_eq!(MyEnum::A.value(), 0);
-///     assert_eq!(MyEnum::B.value(), 1);
+///     // it's prefered to use the function call to `value` 
+///     // to get a [`&'static T`] reference to the value
+///     assert_eq!(MyEnum::A.value(), &0);
+///     assert_eq!(MyEnum::B.value(), &1);
 ///     assert_eq!(Tags::Key.value(), b"\x00\x01\x7f");
 ///     assert_eq!(Tags::Length.value(), b"\xba\x5e");
 ///     assert_eq!(Tags::Data.value(), b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f");
+/// 
+///     // can also check equality without the function call. This must compare the input 
+///     // type defined in `#[armtype = ...]`
+///     assert_eq!(Tags::Length, b"\xba\x5e");
 /// }
 /// ```
 pub fn enum_const(input: TokenStream) -> TokenStream {
+    let name = "EnumConst";
     let input = parse_macro_input!(input as DeriveInput);
     // --------------------------------------------------
     // extract the name, variants, and values
@@ -63,40 +94,60 @@ pub fn enum_const(input: TokenStream) -> TokenStream {
     let enum_name = &input.ident;
     let variants = match input.data {
         Data::Enum(DataEnum { variants, .. }) => variants,
-        _ => panic!("`EnumConst` can only be derived for enums"),
+        _ => panic!("{}", Error::DeriveForNonEnum(name.into())),
     };
     // --------------------------------------------------
     // extract the type
     // --------------------------------------------------
-    let (type_name, deref) = match get_type_deref(&input.attrs) {
+    let (type_name, deref) = match get_deref_type(&input.attrs) {
         Some((type_name, deref)) => (type_name, deref),
-        None => panic!("Missing #[armtype = ...] attribute, required for `EnumConst`-derived enum"),
+        None => panic!("{}", Error::MissingArmType("applied to enum".into(), name.into())),
     };
     let type_name_raw = match get_type(&input.attrs) {
         Some(type_name_raw) => type_name_raw,
-        None => panic!("Missing #[armtype = ...] attribute, required for `EnumConst`-derived enum"),
+        None => panic!("{}", Error::MissingArmType("applied to enum".into(), name.into())),
     };
     // --------------------------------------------------
-    // generate the output tokens and return
+    // generate the output tokens
     // --------------------------------------------------
     let variant_match_arms = variants
         .iter()
         .map(|variant| {
             let variant_name = &variant.ident;
-            let value = match get_val(&variant.attrs) {
+            let value = match get_val(name.into(), &variant.attrs) {
                 Ok(value) => value,
                 Err(e) => panic!("{}", e),
             };
+            // ------------------------------------------------
+            // if the type input is a reference (e.g. &[u8] or &str)
+            // then the return type will be 
+            // * `&'static [u8]` or
+            // * `&'static str`
+            //
+            // otherwise, if the input is not a reference (e.g. u8 or f32)
+            // then the return type will be
+            // * `&'static u8` or
+            // * `&'static f32`
+            //
+            // as a result, need to ensure we are removing / adding
+            // the `&` symbol wherever necessary
+            // ------------------------------------------------
             match deref {
                 true => quote! { #enum_name::#variant_name => #value, },
                 false => quote! { #enum_name::#variant_name => &#value, },
             }
         }
     );
+    // --------------------------------------------------
+    // see deref comment above
+    // --------------------------------------------------
     let variant_par_eq = match deref {
         true => quote! { &self.value() == other },
         false => quote! { self.value() == other },
     };
+    // --------------------------------------------------
+    // return
+    // --------------------------------------------------
     let expanded = quote! {
         impl #enum_name {
             #[inline]
@@ -126,43 +177,78 @@ pub fn enum_const(input: TokenStream) -> TokenStream {
 /// Add's constants of any type to each arm of an enum
 /// 
 /// To get the value, the type must be explicitly passed
-/// as a generic to [`<enum_name>::value`]. Upon failure,
-/// it will return [`None`].
+/// as a generic to [`<enum_name>::value`]. This will automatically
+/// try to convert constant to the expected type using [`std::any::Any`] 
+/// and [`downcast_ref`]. Currently [`TryFrom`] is not supported, so typing
+/// is fairly strict. Upon failure, it will return [`None`].
+/// 
+/// * To get the value as a reference, call the function [`<enum_name>::value`]
+/// * Unlike [`EnumConst`], this macro does not enable direct comparison
+///   using [`PartialEq`].
+/// 
+/// The `#[armtype = ...]` attribute is **NOT*** required for this macro to function, 
+/// but ***CAN** be applied to ***each individual arm*** of the enum, since values
+/// are not expected to share a type. If no type is given, then the type is
+/// inferred from the literal value in the `#[value = ...]` attribute.
+/// 
+/// All values set will return a [`Option<&'static T>`] reference. To the input type,
+/// of [`T`] AND [`&T`]. If multiple references are used (e.g. `&&T`), then
+/// the return type will be [`Option<&'static &T>`].
 /// 
 /// # Example
 /// 
 /// ```
-/// use enum_const::EnumConst;
+/// use enum_const::EnumConstAny;
 /// 
-/// #[derive(EnumConst)]
-/// #[armtype("i32")]
+/// #[derive(EnumConstAny, Debug)]
 /// enum MyEnum {
-///     #[value = 0]
+///     #[armtype(u8)]
+///     #[value = 0xAA]
 ///     A,
-///     #[value = 1]
+///     #[value = "test3"]
 ///     B,
 /// }
 /// 
-/// #[derive(EnumConst)]
-/// #[armtype(&[u8])]
+/// #[derive(EnumConstAny, Debug)]
 /// enum Tags {
-///     #[value = b"\x00\x01\x7f"]
+///     #[value = b"\x00\x01"]
 ///     Key,
-///     #[value = b"\xba\x5e"]
+///     #[armtype(u16)]
+///     #[value = 24250]
 ///     Length,
+///     #[armtype(&[u8])]
 ///     #[value = b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f"]
 ///     Data,
 /// }
 /// 
 /// fn main() {
-///     assert_eq!(MyEnum::A.value(), 0);
-///     assert_eq!(MyEnum::B.value(), 1);
-///     assert_eq!(Tags::Key.value(), b"\x00\x01\x7f");
-///     assert_eq!(Tags::Length.value(), b"\xba\x5e");
-///     assert_eq!(Tags::Data.value(), b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f");
+///     // [`EnumConstAny`] examples
+///     assert!(MyEnum::A.value::<u8>().is_some());
+///     assert!(MyEnum::A.value::<Vec<f32>>().is_none());
+///     assert!(MyEnum::B.value::<u8>().is_none());
+///     assert!(MyEnum::B.value::<&str>().is_some());
+///     assert!(Tags::Data.value::<&[u8]>().is_some());
+/// 
+///     // An infered type. This will be as strict as possible,
+///     // therefore [`&[u8]`] will fail but [`&[u8; 2]`] will succeed
+///     assert!(Tags::Key.value::<&[u8; 2]>().is_some());
+///     assert!(Tags::Key.value::<&[u8; 5]>().is_none());
+///     assert!(Tags::Key.value::<&[u8]>().is_none());
+///     assert!(u16::from_le_bytes(**Tags::Key.value::<&[u8; 2]>().unwrap()) == 0x0100);
+/// 
+///     // casting as anything other than the defined / inferred type will
+///     // fail, since this uses [`downcast_ref`] from [`std::any::Any`]
+///     assert!(Tags::Length.value::<u16>().is_some());
+///     assert!(Tags::Length.value::<u32>().is_none());
+///     assert!(Tags::Length.value::<u64>().is_none());
+/// 
+///     // however, can always convert to a different type
+///     // after value is successfully acquired
+///     assert!(*Tags::Length.value::<u16>().unwrap() as u32 == 24250);
 /// }
 /// ```
 pub fn enum_const_any(input: TokenStream) -> TokenStream {
+    let name = "EnumConstAny";
     let input = parse_macro_input!(input as DeriveInput);
     // --------------------------------------------------
     // extract the name, variants, and values
@@ -170,14 +256,17 @@ pub fn enum_const_any(input: TokenStream) -> TokenStream {
     let enum_name = &input.ident;
     let variants = match input.data {
         Data::Enum(DataEnum { variants, .. }) => variants,
-        _ => panic!("`EnumConstAny` can only be derived for enums"),
+        _ => panic!("{}", Error::DeriveForNonEnum(name.into())),
     };
     // --------------------------------------------------
-    // generate the output tokens and return
+    // generate the output tokens
     // --------------------------------------------------
     let variant_code = variants.iter().map(|variant| {
         let variant_name = &variant.ident;
-        match (get_type(&variant.attrs), get_val(&variant.attrs)) {
+        match (get_type(&variant.attrs), get_val(name.into(), &variant.attrs)) {
+            // ------------------------------------------------
+            // if type is specified, use it
+            // ------------------------------------------------
             (Some(typ), Ok(value)) => quote! {
                 #enum_name::#variant_name => {
                     let val: &dyn ::std::any::Any = &(#value as #typ);
@@ -185,18 +274,27 @@ pub fn enum_const_any(input: TokenStream) -> TokenStream {
                 },
 
             },
+            // ------------------------------------------------
+            // no type specified, try to infer
+            // ------------------------------------------------
             (None, Ok(value)) => quote! {
                 #enum_name::#variant_name => {
                     let val: &dyn ::std::any::Any = &#value;
                     val.downcast_ref::<T>()
                 },
             },
+            // ------------------------------------------------
+            // unable to infer type
+            // ------------------------------------------------
             (_, Err(_)) => quote! { #enum_name::#variant_name => None, },
         }
     });
+    // ------------------------------------------------
+    // return
+    // ------------------------------------------------
     let expanded = quote! {
         impl #enum_name {
-            pub fn value<T: 'static>(&self) -> Option<&T> {
+            pub fn value<T: 'static>(&self) -> Option<&'static T> {
                 match self {
                     #( #variant_code )*
                     _ => None,
@@ -218,7 +316,7 @@ pub fn enum_const_any(input: TokenStream) -> TokenStream {
 /// # Output
 ///
 /// [`TokenStream`] containing the value `<value>`, or [`Err`] if the attribute is not present / invalid
-fn get_val(attrs: &[Attribute]) -> Result<proc_macro2::TokenStream, syn::Error> {
+fn get_val(name: String, attrs: &[Attribute]) -> Result<proc_macro2::TokenStream, Error> {
     for attr in attrs {
         if !attr.path.is_ident("value") { continue; }
         match attr.parse_meta() {
@@ -236,7 +334,7 @@ fn get_val(attrs: &[Attribute]) -> Result<proc_macro2::TokenStream, syn::Error> 
                 Meta::Path(_) => return Ok(meta.into_token_stream())
             },
             Err(_) => {
-                return Err(syn::Error::new_spanned("", "Attemping to parse non-literal attribute for `value`: not yet supported"))
+                return Err(Error::NonLiteralValue);
                 /*
                 // Maybe for future:
                 // --------------------------------------------------
@@ -264,7 +362,7 @@ fn get_val(attrs: &[Attribute]) -> Result<proc_macro2::TokenStream, syn::Error> 
             },
         }
     }
-    Err(syn::Error::new_spanned("", "Missing #[value = ...] attribute, expected for `EnumConst` trait"))
+    Err(Error::MissingValue(name))
 }
 
 /// Helper function to extract the type from the [`Attribute`], aka `#[armtype(<type>)]`
@@ -286,7 +384,7 @@ fn get_val(attrs: &[Attribute]) -> Result<proc_macro2::TokenStream, syn::Error> 
 /// 
 /// * 0 - [`Type`] containing the type `<type>` (already de-referenced)
 /// * 1 - An additional flag that indicates if the type has been de-referenced
-fn get_type_deref(attrs: &[Attribute]) -> Option<(Type, bool)> {
+fn get_deref_type(attrs: &[Attribute]) -> Option<(Type, bool)> {
     for attr in attrs {
         if !attr.path.is_ident("armtype") { continue; }
         let tokens = match attr.parse_args::<proc_macro2::TokenStream>() {
